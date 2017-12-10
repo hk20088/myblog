2bc13aea8ae16990775c2ec544a6d9d49d5ea05d
Leon Hu <927572766@qq.com>
2017-12-10 11:06:21 +0800

    定义response_factory函数

--- www/app.py
+++ www/app.py
@@ -118,6 +118,84 @@ def data_factory(app, handler):
 #         return (yield from handler(request))
 #     return auth
 
+'''
+# 响应处理
+
+总结下来一个请求在服务端收到后的方法调用顺序是:
+ logger_factory->response_factory->RequestHandler().__call__->get或post->handler
+那么结果处理的情况就是:
+   	由handler构造出要返回的具体对象
+   	然后在这个返回的对象上加上'__method__'和'__route__'属性，以标识别这个对象并使接下来的程序容易处理
+   	RequestHandler目的就是从URL函数中分析其需要接收的参数，从request中获取必要的参数，调用URL函数,然后把结果返回给response_factory
+   	response_factory在拿到经过处理后的对象，经过一系列对象类型和格式的判断，构造出正确web.Response对象，以正确的方式返回给客户端
+ 在这个过程中，我们只用关心我们的handler的处理就好了，其他的都走统一的通道，如果需要差异化处理，就在通道中选择适合的地方添加处理代码。
+ 在response_factory中应用了jinja2来套用模板
+'''
+@asyncio.coroutine
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
+        logging.info('Response handler...')
+        # 调用相应的handler处理request
+        r = yield from handler(request)
+        logging.info('r = %s' % str(r))
+        # 如果响应结果为web.StreamResponse类，则直接把它作为响应返回
+        if isinstance(r, web.StreamResponse):
+            return r
+
+        # 如果响应结果为字节流，则把字节流塞到response的body里，设置响应类型为流类型，返回
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+
+        # 如果响应结果为字符串
+        if isinstance(r, str):
+            # 先判断是不是需要重定向，是的话直接用重定向的地址重定向
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            # 不是重定向的话，把字符串当做是html代码来处理
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+
+        # 如果响应结果为字典
+        if isinstance(r, dict):
+            # 先查看一下有没有'__template__'为key的值
+            template = r.get('__template__')
+            # 如果没有，说明要返回json字符串，则把字典转换为json返回，对应的response类型设为json类型
+            if template is None:
+                resp = web.Response(body=json.dumps(
+                    r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                r['__user__'] = request.__user__
+                # 如果有'__template__'为key的值，则说明要套用jinja2的模板，'__template__'Key对应的为模板网页所在位置
+                resp = web.Response(body=app['__templating__'].get_template(
+                    template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                # 以html的形式返回
+                return resp
+
+        # 如果响应结果为Int
+        if isinstance(r, int) and 100 <= r < 600:
+            return web.Response(r)
+
+        # 如果响应结果为tuple且数量为2
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            # 如果tuple的第一个元素是int类型且在100到600之间，这里应该是认定为t为http状态码，m为错误描述
+            # 或者是服务端自己定义的错误码+描述
+            if isinstance(t, int) and 100 <= t < 600:
+                return web.Response(status=t, text=str(m))
+            # default: 默认直接以字符串输出
+            resp = web.Response(body=str(r).encode('utf-8'))
+            resp.content_type = 'text/plain;charset=utf-8'
+            return resp
+
+    return response
+
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
