7e108a5bdbd67b78d4869b609e5af8ba0fc742b8
Leon Hu <927572766@qq.com>
2017-12-12 10:01:51 +0800

    新增用户注册，登录函数

--- www/handlers.py
+++ www/handlers.py
@@ -10,26 +10,159 @@ logging.basicConfig(level=logging.INFO)
 import time
 import asyncio
 import hashlib
+import re
+import json
 
+from aiohttp import web
 from www.coroweb import get, post
-from www.models import User
+from www.models import User, Blog, Comment, next_id
 from www.config import configs
+from www.apis import APIValueError, APIPermissionError, APIError
+
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__':'test.html',
+#         'users':users
+#     }
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__':'test.html',
-        'users':users
+        '__template__':'blogs.html',
+        'blogs':blogs
     }
+
+@get('/register')
+def register():
+    return {
+        '__template__':'register.html'
+    }
+
+@get('/signin')
+def signin():
+    logging.info('被调用了呀。。。')
+    return {
+        '__template__':'signin.html'
+    }
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+# 注册时，服务器收到的密码是在客户端经过SHA加密过的密码
+@post('/api/userRegister')
+@asyncio.coroutine
+def api_register_user(*, email, name, passwd):
+    # 判断name是否存在，且是否只是'\n', '\r',  '\t',  ' '，这种特殊字符
+    if not name or not name.strip():
+        raise APIValueError('name')
+    # 判断email是否存在，且是否符合规定的正则表达式
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    # 判断passwd是否存在，且是否符合规定的正则表达式
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+
+    # 查一下库里是否有相同的email地址，如果有的话提示用户email已经被注册过
+    users = yield from User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+
+    # 生成一个当前要注册用户的唯一uid
+    uid = next_id()
+    # 构建shal_passwd
+    sha1_passwd = '%s:%s' % (uid, passwd)
+
+    admin = False
+    if email == 'admin@163.com':
+        admin = True
+
+    # 创建一个用户，密码是用sha1加密保存
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest(),
+                admin=admin)
+    # 保存这个用户到数据库
+    yield from user.save()
+    logging.info('save user ok...')
+    # 构建返回信息
+    r = web.Response()
+
+    # 添加cookie
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    # 把密码改为 ******，以避免返回的时候暴露
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    # 把对象转换了json格式返回
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+# 登录请求，切记， 同一个模块不可以有重名的函数
+@post('/api/authenticate')
+@asyncio.coroutine
+def authenticate(*, email, passwd):
+    # 如果email或passwd为空，都说明有错误
+    if not email:
+        raise APIValueError('email', 'Invalid email')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid  passwd')
+    # 根据email在库里查找匹配的用户
+    users = yield from User.findAll('email=?', [email])
+    # 没找到用户，返回用户不存在
+    if len(users) == 0:
+        raise APIValueError('email', 'email not exist')
+    # 取第一个查到用户，理论上就一个
+    user = users[0]
+    # 按存储密码的方式，取出传入的密码的hash值
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    # 和库里的密码字段的值作比较，一样的话认证成功，不一样的话，认证失败
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid passwd')
+
+    # 构建返回信息
+    r = web.Response()
+    # 添加cookie
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    # 只把要返回的实例的密码改成'******'，库里的密码依然是正确的，以保证真实的密码不会因返回而暴漏
+    user.passwd = '******'
+    # 返回的是json数据，所以设置content-type为json的
+    r.content_type = 'application/json'
+    # 把对象转换成json格式返回
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+
+
+
+
+
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
+# 根据用户信息，拼接一个cookie字符串
+def user2cookie(user, max_age):
+    # 过期时间是当前时间+设置的有效时间
+    expires = str(int(time.time() + max_age))
+    # 构建cookie存储的字符串信息
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    # 将L用 - 隔开并返回
+    return '-'.join(L)
 
-# 根据cookie字符串，解析出用户信息相关的
 
-
+# 根据cookie字符串，解析出用户相关信息
 @asyncio.coroutine
 def cookie2user(cookie_str):
     # cookie_str是空则返回
