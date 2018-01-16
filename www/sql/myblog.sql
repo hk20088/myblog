8f92dd58b752173b34018d450beb58ba7e75c796
Leon Hu <927572766@qq.com>
2018-01-16 12:08:12 +0800

    blogs表加了priority字段，数值越大优先级越高。显示越靠前。 为做置顶博客功能做扩展。

--- www/sql/myblog.sql
+++ www/sql/myblog.sql
@@ -28,6 +28,7 @@ create table blogs (
     `summary` varchar(200) not null,
     `content` mediumtext not null,
     `created_at` real not null,
+    `priority` varchar(2) not null,
     key `idx_created_at` (`created_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
